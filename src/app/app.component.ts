62acd9aa4f2c9f493c69125e38366ddd7d5bf269
tauhid234 <syifatauhid@gmail.com>
2021-05-12 18:43:07 +0700

    PROGRESS EID 120521

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ExportAsConfig, ExportAsService } from 'ngx-export-as';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  [x: string]: any;
   title = 'idul-fitri';
+
+  url : String | ArrayBuffer;
+  input : any;
+
+  button : Boolean = false;
+
+  data : any;
+  
+  constructor(private exportAsService : ExportAsService, private toastr : ToastrService){}
+
+  exportAsConfig: ExportAsConfig = {
+    type: 'png', // the type you want to download
+    elementIdOrContent: 'image', // the id of html/table element
+  }
+  
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+        this.button = true;
+      }
+    }
+  }
+  reset(){
+    this.url = null;
+    this.button = false;
+  }
+
+  download() {
+    this.toastr.success("Gambar berhasil di Download","Sukses");
+    // download the file using old school javascript method
+    this.exportAsService.save(this.exportAsConfig, 'save_image').subscribe(() => {
+      // save started
+    });
+
+    this.exportAsService.get(this.config).subscribe(content => {
+      console.log(content);
+    });
+  }
+
+  onKeyUp(x){
+    this.data = x.target.value;
+    console.log(this.data);
+    if(this.url !== "./assets/images/unknown_person.png"){
+      this.button = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ExportAsModule } from 'ngx-export-as';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    ExportAsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
